25712dcfa6729408633f9e40e6c2931c012f6528
AlexSpandex <78000095+AlexSpandex@users.noreply.github.com>
2023-04-29 22:56:14 -0700

    Updated sign in page and index page

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
+
+// POST request to submit discussion post
+app.post('/submit-discussion-post', (req, res) => {
+  const { subject, question } = req.body;
+
+  // Save subject and question to database or other storage
+
+  // Return success message to frontend
+  res.status(200).json({ message: 'Discussion post submitted successfully' });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
